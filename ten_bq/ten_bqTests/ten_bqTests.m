49ad455c58194952745cfdd92e2727ef4a40fe57
tens04 <tens04@gh.local>
2016-08-22 15:06:20 +0800

    第一次

---
+++ ten_bq/ten_bqTests/ten_bqTests.m
@@ -0,0 +1,39 @@
+//
+//  ten_bqTests.m
+//  ten_bqTests
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 tens04. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ten_bqTests : XCTestCase
+
+@end
+
+@implementation ten_bqTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
